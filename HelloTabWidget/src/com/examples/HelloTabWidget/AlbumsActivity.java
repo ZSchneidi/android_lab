837173a33aed63c1f41e69b7f7729e9727deaa73
Schneidi <schneidi-@gmx.de>
2011-07-14 16:26:56 +0200

    init

---
+++ HelloTabWidget/src/com/examples/HelloTabWidget/AlbumsActivity.java
@@ -0,0 +1,13 @@
+package com.examples.HelloTabWidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AlbumsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.inputtab);
+    }
+}
